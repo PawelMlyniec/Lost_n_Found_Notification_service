29f0862cabb6a4d24e083b580ad3cf243d3b5d31
Radosław Panuszewski <radoslaw.panuszewski15@gmail.com>
2021-04-04 00:36:31 +0200

    Add missing getter

--- src/main/java/com/pw/ns/domain/NotificationId.java
+++ src/main/java/com/pw/ns/domain/NotificationId.java
@@ -1,11 +1,18 @@
 package com.pw.ns.domain;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 
 @Data
 @AllArgsConstructor(staticName = "of")
 public class NotificationId {
 
+    @Getter(AccessLevel.NONE)
     private final String id;
+
+    public String raw() {
+        return id;
+    }
 }
